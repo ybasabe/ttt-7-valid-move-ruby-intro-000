064b990c10ac4fec4b93dca15421af82190ebaea
dazzling-directory-8519 <yamirabasabe@gmail.com>
2020-01-28 17:58:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,6 @@
 
 
 
-def valid_move?(board, position)
-  move = position.to_i
-  
-  test = move-1
-  
-  
-  if position_taken?(board, test) == false && test.between?(0, 8)
-    TRUE
-    
-  else
-    false
-    
-  end
-end
 
 
 
